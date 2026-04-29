cbc6dfad8e4185485e764aa005dae16d22009de4
Urmilaa <urmikher@gmail.com>
2026-04-29 22:39:37 +0530

    Add VPC and EKS modules to main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,48 @@
+#####################################
+# VPC MODULE
+#####################################
 
-#provider "aws" {
- #region = "us-east-1"  
-#}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
 
-#This is s3 bucket
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 
-# variable "bucket_names" {
-#  default = [
- #   "terraform-for-devops-urmi-new2",
-  #  "terraform-for-devops-urmi-new3",
-  #  "terraform-for-devops-urmi-new4"
- # ]
-#}
+  azs = ["ap-south-1a", "ap-south-1b"]
 
-#resource "aws_s3_bucket" "buckets" {
- # for_each = toset(var.bucket_names)
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
- # bucket = each.value
-#}
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+}
+
+#####################################
+# EKS MODULE
+#####################################
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+
+      instance_types = ["t3.micro"]
+    }
+  }
+}
