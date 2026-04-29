0764b94d62cbcc064cfc96c5b060131a59fc4fbb
Urmilaa <urmikher@gmail.com>
2026-04-29 23:04:07 +0530

    Update Terraform provider version and required version

--- provider.tf
+++ provider.tf
@@ -1,10 +1,14 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_version = ">= 1.3.0"
 
-required_providers {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = "~> 5.0"
     }
   }
+}
+
+provider "aws" {
+  region = var.region
 }
