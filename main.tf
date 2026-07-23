945328189be509587dcfbd8c724b5327c2398d22
Urmilaa <urmikher@gmail.com>
2026-07-23 12:37:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,13 @@ module "vpc" {
   single_nat_gateway = true
 
   enable_dns_hostnames = true
+
+  tags = {
+    Terraform  = "true"
+    Environment = "Dev"
+    Project     = "EKS-Lab"
+  }
+
 }
 
 #####################################
@@ -26,31 +33,58 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  version = "20.37.2"
 
-  name               = var.cluster_name
-  kubernetes_version = "1.33"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.30"
+
+  ###################################
+  # Networking
+  ###################################
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  endpoint_public_access  = true
-  endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  ###################################
+  # Authentication
+  ###################################
 
   enable_cluster_creator_admin_permissions = true
 
+  ###################################
+  # IRSA
+  ###################################
+
   enable_irsa = true
 
+  ###################################
+  # Managed Node Group
+  ###################################
+
   eks_managed_node_groups = {
+
     default = {
+
       desired_size = 2
       min_size     = 1
       max_size     = 3
 
       instance_types = ["t3.micro"]
-      ami_type       = "AL2023_x86_64_STANDARD"
+
+      ami_type = "AL2023_x86_64_STANDARD"
+
+      capacity_type = "ON_DEMAND"
+
+      disk_size = 20
     }
   }
+
+  ###################################
+  # Tags
+  ###################################
 
   tags = {
     Environment = "Dev"
