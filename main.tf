0d1bacfe91da1a2475d7822793c05a0d7dd3d188
Urmilaa <urmikher@gmail.com>
2026-07-23 14:49:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "eks" {
   version = "20.37.2"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.31"
 
   ###################################
   # Networking
